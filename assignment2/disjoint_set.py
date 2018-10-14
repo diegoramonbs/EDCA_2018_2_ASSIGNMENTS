cf51af5b8c059012ffca9df6ca301d77ae6fc319
Diego Ramon <diego.ramon@eng.ci.ufpb.br>
2018-10-14 17:10:11 -0300

    Finalizado algoritmo knapsack

--- assignment2/disjoint_set.py
+++ assignment2/disjoint_set.py
@@ -2,23 +2,32 @@
 # -*- coding: utf-8 -*-
 
 class DisjointSet(object):
+
     def __init__(self, size):
+        # Create a new element with a unique id, a rank of 0
         self.rank = [0]*size
+
+        # The parent pointer to itself indicates that element is the representative member of its own set
         self.parent = [i for i in range(size)]
         self.size = size
 
     def find(self, x):
+        # It follows the chain of parent pointers from x up the tree until it reaches a root element
         if self.parent[x] != x:
             self.parent[x] = self.find(self.parent[x])
         return self.parent[x]
 
     def union(self, x, y):
+        # Determine the roots of the trees x and y belong to
         x_root = self.find(x)
         y_root = self.find(y)
 
+        #If the roots are not distinct
         if x_root == y_root:
             return
 
+        # If the roots are distinct, the trees are combined by attaching the root of one
+        # to the root of the other.
         if self.rank[x_root] < self.rank[y_root]:
             self.parent[x_root] = y_root
 
